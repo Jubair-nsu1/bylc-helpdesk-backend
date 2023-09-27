63a300be7c23c22abefef95df3f31df8d7c66cd1
Jubair Ahmed <62503637+Jubair-nsu1@users.noreply.github.com>
2023-09-27 21:36:22 +0600

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ const connectDB = require('./config/db')
 const app = express();
 app.use(express.urlencoded({ extended : false }) );
 app.use(express.json());
-app.use(helmet());
 app.use(cors());
 
 
